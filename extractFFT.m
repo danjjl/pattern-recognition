c8a2e0e80e1543f75a4de9c674c6e0f988dc942b
Jonathan Dan <danjjl@hotmail.com>
2014-12-04 13:06:37 +0100

    Fixed FFT calculation

--- extractFFT.m
+++ extractFFT.m
@@ -1,12 +1,11 @@
-function featData = extractFFT(rdata, numfeatures)
+function featData = extractFFT(rdata, numFeatures)
 %FFT from continuous normalized chaincode feature extractor 
     %TODO TO MUCH CODE duplication from featExtract 
-    grey_data = im_gray(rdata); % not necessary with this data, already greyscale
-    box_data  = im_box(grey_data,1,0); %remove empty empty border columns and rows 
+    box_data  = im_box(rdata,1,0); %remove empty empty border columns and rows 
     p36_data  = im_resize(box_data,[36 36]); % resize to 36*36
     dataset   = prdataset(p36_data);%convert to dataset
     featData  = [];
-    parfor i=1:size(rdata,1)
+    for i=1:size(rdata,1)
         BWlist   = contour(data2im(dataset,i));%get contour coordinate
         ChainC   = chaincode(BWlist); % get chaincode
         CChainC = continuousChain(ChainC); %continous chaincode (no jumps from 8 <-> 0)
@@ -28,19 +27,19 @@ function featData = extractFFT(rdata, numfeatures)
         j=1;
         k=1;
         Y = 0;
-        f = freq(1);
-        while freq(j) < 0
-          %freqs aren't in exact same position (beacuse N varies) but very close so consider first freq, second, ... (not exact value)
+        fH = -0.5 + 0.5/numFeatures;
+        for k=1:numFeatures
+            Y(k) = 0;
+        end
+        k=1;
+        while k < numFeatures
+          %Trapeze calculation
           Y(k) =  Y(k) + X(j)*(freq(j+1)-freq(j)) + 0.5*(freq(j+1)-freq(j))*(X(j+1)-X(j));
-          if abs(f(k)-freq(j)) > 0.5/numfeatures;%integration period
+          if freq(j) > fH;%integration period
               k = k+1;
-              f(k) = freq(j);
-              Y(k) = 0;
+              fH = -0.5 + 0.5/numFeatures;
           end
           j = j+1;
         end
-        %FIX FIND why sometimes length = 1!
-        if length(Y) >= numfeatures
-            featData = [featData; prdataset(Y(1:numfeatures), getlab(dataset(i,:)))]; %save in dataset (numfeatures checks same amount features saved (more debug option))
-        end
+        featData = [featData; prdataset(Y(1:numFeatures), getlab(dataset(i,:)))]; %save in dataset (numfeatures checks same amount features saved (more debug option))
     end
